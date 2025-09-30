d6358f3d40dc4e20addcf50a9a30b4b7ffdf0ab6
Iris Felshman <iris.felshman@gmail.com>
2025-09-30 15:45:40 -0500

    cleaning up the math/logic here

--- day_36/stock-news-extrahard-start/main.py
+++ day_36/stock-news-extrahard-start/main.py
@@ -29,22 +29,35 @@ def get_stock():
     }
     stock_request = requests.get(stock_url, stock_params)
     stock_request_json = stock_request.json()
-    # Now that we have the data, we process it
-    prevday_close = None
-    for day in list(time_series.items())[:2]: # Cut down to the last two items so we only check the last two days.
-        day_close = float(day[1]['4. close'])
-        if prevday_close != None:
-            difference = abs(prevday_close - day_close)
-            if (difference / prevday_close ) > 0.05: # If difference is larger than 5 percent, we make our message.
-                if prevday_close > day_close:
-                    updown = down_arrow
-                else:
-                    updown = up_arrow
-                # print("hit on", day)
-                news = get_news(day[0]) #Grab and return news articles for that day.
-                return [updown, int(difference)], news
+    # Now that we have the data, we process it. grab the current day and the previous close day values and do some math. here's also sample data if we want to test this working without putting API values in.
+    # sample_data = {'Meta Data': {'1. Information': 'Daily Prices (open, high, low, close) and Volumes', '2. Symbol': 'INTL', '3. Last Refreshed': '2025-09-29', '4. Output Size': 'Compact', '5. Time Zone': 'US/Eastern'}, 'Time Series (Daily)': {'2025-09-29': {'1. open': '27.8100', '2. high': '27.8200', '3. low': '27.6700', '4. close': '27.7583', '5. volume': '15073'}, '2025-09-26': {'1. open': '27.4900', '2. high': '27.6400', '3. low': '27.4600', '4. close': '27.6200', '5. volume': '32328'}}}
+    current_day = next(iter(stock_request_json['Time Series (Daily)']))
+    days = [f for f in list(stock_request_json['Time Series (Daily)'].values())[:2]]
+    day_close = float(days[0]['4. close'])
+    prevday_close = float(days[1]['4. close'])
+    difference = int(100*(abs(prevday_close - day_close) / prevday_close))
+    if difference > 5 and day_close>prevday_close:
+        return [up_arrow, difference], get_news(current_day)
+    elif difference > 5 and day_close < prevday_close:
+        return [down_arrow, difference], get_news(current_day)
+    else:
+        return None
+    # It was like this but I felt this was messy so I went back and fixed it. YMMV.
+    # prevday_close = None
+    # for day in list(time_series.items())[:2]: # Cut down to the last two items so we only check the last two days.
+    #     day_close = float(day[1]['4. close'])
+    #     if prevday_close != None:
+    #         difference = abs(prevday_close - day_close)
+    #         if (difference / prevday_close ) > 0.05: # If difference is larger than 5 percent, we make our message.
+    #             if prevday_close > day_close:
+    #                 updown = down_arrow
+    #             else:
+    #                 updown = up_arrow
+    #             # print("hit on", day)
+    #             news = get_news(day[0]) #Grab and return news articles for that day.
+    #             return [updown, int(difference)], news
 
-        prevday_close = day_close
+    #     prevday_close = day_close
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
@@ -80,11 +93,5 @@ def send_whatsapp(text):
     )
 
 info = get_stock()
-send_whatsapp(f"{STOCK}: {info[0][0]} {info[0][1]}%\nHeadline: {info[1][0]['title']}\nBrief:{info[1][0]['headline']}\nURL:{info[1][0]['url']}")
-# sample_data = {'Meta Data': {'1. Information': 'Daily Prices (open, high, low, close) and Volumes', '2. Symbol': 'INTL', '3. Last Refreshed': '2025-09-29', '4. Output Size': 'Compact', '5. Time Zone': 'US/Eastern'}, 'Time Series (Daily)': {'2025-09-29': {'1. open': '27.8100', '2. high': '27.8200', '3. low': '27.6700', '4. close': '27.7583', '5. volume': '15073'}, '2025-09-26': {'1. open': '27.4900', '2. high': '27.6400', '3. low': '27.4600', '4. close': '27.6200', '5. volume': '32328'}}}
-# print(sample_data['Time Series (Daily)']['2025-09-29']['4. close'])
-# for item in sample_data['Time Series (Daily)']:
-#     print(sample_data['Time Series (Daily)'][item])
-
-# result=[value for (key, value) in sample_data['Time Series (Daily)'].items()]
-# print(result)
+if info != None:
+    send_whatsapp(f"{STOCK}: {info[0][0]} {info[0][1]}%\nHeadline: {info[1][0]['title']}\nBrief:{info[1][0]['headline']}\nURL:{info[1][0]['url']}")
